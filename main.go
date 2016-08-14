ba36c293bb8fba74df082323cc7300b18f371e39
Jakub Matyka <kmatyka@ntrc.eu>
2016-08-14 23:45:05 +0200

    add handlers for different urls

--- main.go
+++ main.go
@@ -12,9 +12,23 @@ func main() {
 
 	// On the default page we will simply serve our static index page.
 	r.Handle("/", http.FileServer(http.Dir("./views/")))
+	// Our API is going to consist of three routes
+	// /status - which we will call to make sure that our API is up and running
+	// /products - which will retrieve a list of products that the user can leave feedback on
+	// /products/{slug}/feedback - which will capture user feedback on products
+	r.Handle("/status", NotImplemented).Methods("GET")
+	r.Handle("/products", NotImplemented).Methods("GET")
+	r.Handle("/products/{slug}/feedback", NotImplemented).Methods("POST")
 	// We will setup our server so we can serve static assets like images, css from the /static/{file} route
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 
 	// Our application will run on port 3000. Here we declare the port and pass in our router.
 	http.ListenAndServe(":3000", r)
 }
+
+// Here we are implementing the NotImplemented handler. Whenever an API endpoint is hit
+// we will simply return the message "Not Implemented"
+var NotImplemented = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Not Implemented"))
+})
+
